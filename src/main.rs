75f80041167bdda8fc95f07f007298de056df34f
0leg0602 <olegpolyakov0602@gmail.com>
2026-03-02 22:37:11 -0500

    add pawn, add mutlicolor support

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,31 @@ struct BoardPart;
 #[derive(Component)]
 struct ChessPieces;
 
-#[derive(Resource)]
-struct AnimationTimer(Timer);
+#[derive(Component)]
+struct Colored;
+
+#[derive(Component)]
+enum PieceColor{
+    White,
+    Black
+}
 
 #[derive(Resource)]
 struct SelectedPiece(Option<Entity>);
+
+#[derive(Resource)]
+struct ChessMaterials{
+    white: Handle<StandardMaterial>,
+    black: Handle<StandardMaterial>,
+}
 
 struct MainPlugin;
 
 impl Plugin for MainPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(AnimationTimer(Timer::from_seconds(1_f32, TimerMode::Repeating)));
         app.insert_resource(SelectedPiece(None));
-        app.add_systems(Startup, create);
-        app.add_systems(Update, (keyboard_update));
+        app.add_systems(Startup, (setup_materials, create).chain());
+        app.add_systems(Update, (keyboard_update, update_textures));
     }
 }
 
@@ -33,7 +44,36 @@ fn main() {
     .run();
 }
 
-fn create(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
+fn setup_materials(mut commands: Commands, mut materials: ResMut<Assets<StandardMaterial>>) {
+    commands.insert_resource(ChessMaterials{
+        white: materials.add(Color::WHITE),
+        black: materials.add(Color::BLACK),
+    });
+}
+
+fn update_textures(
+    pieces_query: Query<(Entity, &PieceColor), (With<ChessPieces>, Without<Colored>)>,
+    mut commands: Commands,
+    mesh_query: Query<Entity, With<MeshMaterial3d<StandardMaterial>>>,
+    children_query: Query<&Children>,
+    chess_material: Res<ChessMaterials>,
+){
+    
+    for (entity, color) in pieces_query{
+        for child in children_query.iter_descendants(entity){
+            if mesh_query.get(child).is_ok() {
+                    match color {
+                        PieceColor::White => {commands.entity(child).insert(MeshMaterial3d(chess_material.black.clone()));},
+                        PieceColor::Black => {commands.entity(child).insert(MeshMaterial3d(chess_material.white.clone()));},
+                    }
+
+                    commands.entity(entity).insert(Colored);
+            }
+        }
+    }
+}
+
+fn create(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>, asset_server: Res<AssetServer>) {
     commands.spawn((
         Transform::default(),
         Rotator,
@@ -44,11 +84,14 @@ fn create(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut material
 
     ));
 
+    let pawn_handle = asset_server.load("models/pawn.glb#Scene0");
+    
+
     let board_size = 8;
     let offset_size = 0.1;
 
-    for col in 0..board_size{
-        for row in 0..board_size{
+    for col in 0..board_size {
+        for row in 0..board_size {
             let mut part_color = Color::srgb(0.0, 0.0, 0.0);
             if (row+col) % 2 != 0 {
                 part_color = Color::srgb(1.0, 1.0, 1.0);
@@ -57,20 +100,22 @@ fn create(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut material
             let board_part_y = (((board_size-1) as f32 + (board_size-1) as f32 * offset_size)/2.0) - (1.0 + offset_size) * col as f32;
 
             if row < 2 || row > 5 {
-                    let piece_color;
+                    
+                let mut chess_piece = commands.spawn((
+                    SceneRoot(pawn_handle.clone()),
+                    Transform::from_xyz(board_part_x, 0.55, board_part_y),
+                    ChessPieces,
+                ));
 
-                    if row > 5 {
-                        piece_color  = Color::srgb(0.8, 0.8, 0.8);
-                    } else {
-                        piece_color  = Color::srgb(0.2, 0.2, 0.2);
-                    }
+                chess_piece.observe(handle_click);
 
-                    commands.spawn((
-                        ChessPieces,
-                        Mesh3d(meshes.add(Cuboid::new(0.6, 1.0, 0.6))),
-                        MeshMaterial3d(materials.add(piece_color)),
-                        Transform::from_xyz(board_part_x, 1.0, board_part_y)
-                    )).observe(handle_click);
+                if row < 2 {
+                    chess_piece.insert(PieceColor::White);
+                } else if row > 5 {
+                    chess_piece.insert(PieceColor::Black);
+                }
+
+
             }
 
             commands.spawn((
@@ -79,6 +124,7 @@ fn create(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut material
                 MeshMaterial3d(materials.add(part_color)),
                 Transform::from_xyz(board_part_x, 0.0, board_part_y)
             )).observe(handle_click);
+
         }
     }
     
@@ -105,11 +151,9 @@ fn keyboard_update(
 
         } if keyboard_input.pressed(KeyCode::KeyW) || keyboard_input.pressed(KeyCode::ArrowUp) {
             camera.rotate_around(Vec3::ZERO, Quat::from_rotation_x(-2.0 * time.delta_secs()));
-            // camera.rotate
 
         } else if keyboard_input.pressed(KeyCode::KeyS) || keyboard_input.pressed(KeyCode::ArrowDown){
             camera.rotate_around(Vec3::ZERO, Quat::from_rotation_x(2.0 * time.delta_secs()));
-            // camera.up();
         }
     }
 }
@@ -133,7 +177,7 @@ fn handle_click(
         if let Ok([hover_transform, mut selected_transform]) = transform_query.get_many_mut([hovered_entity, selected_piece_enity]){
             selected_transform.translation.x = hover_transform.translation.x;
             selected_transform.translation.z = hover_transform.translation.z;
-            selected_transform.translation.y = 1.0;
+            selected_transform.translation.y = 0.55;
 
             selected_piece.0 = None;
         }
